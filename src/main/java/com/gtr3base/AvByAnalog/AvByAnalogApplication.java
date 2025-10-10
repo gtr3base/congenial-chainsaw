52e44e768fa2a27d9bb3685dd78f3a21c6bda2bf
gtr3base <aloxaryabov@gmail.com>
2025-10-10 17:04:41 +0300

    init

---
+++ src/main/java/com/gtr3base/AvByAnalog/AvByAnalogApplication.java
@@ -0,0 +1,13 @@
+package com.gtr3base.AvByAnalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvByAnalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvByAnalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtr3base/AvByAnalog/AvByAnalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtr3base.AvByAnalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvByAnalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
