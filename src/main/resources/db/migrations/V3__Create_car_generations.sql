1c305799de4d0ba9505f7b175ca77325fa5ed541
gtr3base <aloxaryabov@gmail.com>
2025-10-15 16:52:32 +0300

    Migration edits

--- src/main/resources/db/migrations/V3__Create_car_generations.sql
+++ src/main/resources/db/migrations/V3__Create_car_generations.sql
@@ -3,6 +3,8 @@ SET search_path TO av_schema, public;
 CREATE TABLE car_generations(
                                 id SERIAL PRIMARY KEY,
                                 model_id INT REFERENCES car_models(id) ON DELETE CASCADE,
+                                year_start INT NOT NULL,
+                                year_end INT,
                                 name VARCHAR(50) NOT NULL,
                                 UNIQUE(model_id, name)
 );
