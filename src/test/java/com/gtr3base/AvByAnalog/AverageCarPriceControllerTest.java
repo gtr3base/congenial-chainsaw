0788650f9c3e3a54da3767e9c4879c0ff492d273
gtr3base <aloxaryabov@gmail.com>
2026-02-04 15:01:13 +0300

    Update service and test files from feature/average-car-price-service

--- src/test/java/com/gtr3base/AvByAnalog/AverageCarPriceControllerTest.java
+++ src/test/java/com/gtr3base/AvByAnalog/AverageCarPriceControllerTest.java
@@ -1,9 +1,12 @@
 package com.gtr3base.AvByAnalog;
 
 import com.gtr3base.AvByAnalog.controller.AverageCarPriceController;
-import com.gtr3base.AvByAnalog.dto.filter.AverageCarPriceSearchFilter;
+import com.gtr3base.AvByAnalog.dto.AverageCarPriceSearchFilter;
+import com.gtr3base.AvByAnalog.dto.AveragePriceResponse;
 import com.gtr3base.AvByAnalog.security.JwtAuthFilter;
 import com.gtr3base.AvByAnalog.service.AverageCarPriceService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Slf4j
 @WebMvcTest(
         controllers = AverageCarPriceController.class,
         excludeFilters = @ComponentScan.Filter(
@@ -39,11 +43,27 @@ public class AverageCarPriceControllerTest {
     @MockitoBean
     private AverageCarPriceService averageCarPriceService;
 
-    @Test
-    void searchCars_ShouldReturnAverageCarPrice_WhenRequestIsValid() throws Exception {
-        BigDecimal response = new BigDecimal("12850.99");
+    private AveragePriceResponse avgCarPriceResponse;
+    private AveragePriceResponse failureCarPriceResponse;
+    private final BigDecimal response = BigDecimal.valueOf(25000.50);
 
-        when(averageCarPriceService.getAverageCarPrice(any(AverageCarPriceSearchFilter.class))).thenReturn(response);
+    @BeforeEach
+    void setUp() {
+        avgCarPriceResponse = AveragePriceResponse.builder()
+                .averagePrice(response)
+                .success(true)
+                .build();
+
+        failureCarPriceResponse = AveragePriceResponse.builder()
+                .averagePrice(null)
+                .success(false)
+                .build();
+    }
+
+    @Test
+    void searchCars_ShouldReturnTrue_WhenEnoughDataExists() throws Exception {
+        when(averageCarPriceService.getAverageCarPrice(any(AverageCarPriceSearchFilter.class)))
+                .thenReturn(avgCarPriceResponse);
 
         mockMvc.perform(get("/api/average-car-price")
                     .param("carModel", "F30 3-series")
@@ -51,7 +71,25 @@ public class AverageCarPriceControllerTest {
                     .param("year", "2024"))
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(jsonPath("$").value(response));
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.averagePrice").value(25000.50));
+
+        verify(averageCarPriceService, times(1)).getAverageCarPrice(any(AverageCarPriceSearchFilter.class));
+    }
+
+    @Test
+    void searchCars_ShouldReturnFalse_WhenNotEnoughDataExists() throws Exception {
+        when(averageCarPriceService.getAverageCarPrice(any(AverageCarPriceSearchFilter.class)))
+            .thenReturn(failureCarPriceResponse);
+
+        mockMvc.perform(get("/api/average-car-price")
+                    .param("carModel", "F30 3-series")
+                    .param("carMake", "BMW")
+                    .param("year", "2024"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.averagePrice").doesNotExist());
 
         verify(averageCarPriceService, times(1)).getAverageCarPrice(any(AverageCarPriceSearchFilter.class));
     }
