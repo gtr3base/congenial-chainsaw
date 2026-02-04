af198f1bcfa6a8ae6a68f80e909fd4479541bd2c
gtr3base <aloxaryabov@gmail.com>
2026-02-04 14:35:46 +0300

    avg car price business logics fix + dto

--- src/main/java/com/gtr3base/AvByAnalog/controller/AverageCarPriceController.java
+++ src/main/java/com/gtr3base/AvByAnalog/controller/AverageCarPriceController.java
@@ -1,14 +1,13 @@
 package com.gtr3base.AvByAnalog.controller;
 
 import com.gtr3base.AvByAnalog.dto.AverageCarPriceSearchFilter;
+import com.gtr3base.AvByAnalog.dto.AveragePriceResponse;
 import com.gtr3base.AvByAnalog.service.AverageCarPriceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/average-car-price")
@@ -20,7 +19,7 @@ public class AverageCarPriceController {
     }
 
     @GetMapping
-    public ResponseEntity<BigDecimal> getAverageCarPrice(@ModelAttribute AverageCarPriceSearchFilter filter) {
+    public ResponseEntity<AveragePriceResponse> getAverageCarPrice(@ModelAttribute AverageCarPriceSearchFilter filter) {
         return ResponseEntity.ok(averageCarPriceService.getAverageCarPrice(filter));
     }
 }
--- src/main/java/com/gtr3base/AvByAnalog/service/AverageCarPriceService.java
+++ src/main/java/com/gtr3base/AvByAnalog/service/AverageCarPriceService.java
@@ -1,6 +1,7 @@
 package com.gtr3base.AvByAnalog.service;
 
 import com.gtr3base.AvByAnalog.dto.AverageCarPriceSearchFilter;
+import com.gtr3base.AvByAnalog.dto.AveragePriceResponse;
 import com.gtr3base.AvByAnalog.dto.CarSpecification;
 import com.gtr3base.AvByAnalog.entity.Car;
 import com.gtr3base.AvByAnalog.repository.CarRepository;
@@ -11,19 +12,35 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
 public class AverageCarPriceService {
     private final CarRepository carRepository;
 
-    public BigDecimal getAverageCarPrice(AverageCarPriceSearchFilter filter) {
+    private static final int MIN_CARS_FOR_STATISTICS = 5;
+
+    public AveragePriceResponse getAverageCarPrice(AverageCarPriceSearchFilter filter) {
         Specification<Car> spec = CarSpecification.getSpecs(filter);
 
         List<Car> cars =  carRepository.findAll(spec);
 
-        return cars.stream()
+        if(cars.size() < MIN_CARS_FOR_STATISTICS){
+            return AveragePriceResponse.builder().averagePrice(null).success(false).build();
+        }
+
+        BigDecimal sum = cars.stream()
                 .map(Car::getPrice)
-                .reduce(BigDecimal.ZERO, BigDecimal::add).divide(BigDecimal.valueOf(2), RoundingMode.HALF_UP);
+                .filter(Objects::nonNull)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal avg = sum.divide(
+                BigDecimal.valueOf(cars.size()),
+                2,
+                RoundingMode.HALF_UP
+        );
+
+        return AveragePriceResponse.builder().averagePrice(avg).success(true).build();
     }
 }
