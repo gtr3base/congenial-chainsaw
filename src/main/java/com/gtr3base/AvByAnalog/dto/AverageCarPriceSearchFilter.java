053144f34e35f2bd72c66c288e2a99b26d7057d3
gtr3base <aloxaryabov@gmail.com>
2026-02-04 14:36:43 +0300

    format

--- src/main/java/com/gtr3base/AvByAnalog/dto/AverageCarPriceSearchFilter.java
+++ src/main/java/com/gtr3base/AvByAnalog/dto/AverageCarPriceSearchFilter.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class    AverageCarPriceSearchFilter{
+public class AverageCarPriceSearchFilter{
     private String carModel;
     private String carMake;
     private String generation;
